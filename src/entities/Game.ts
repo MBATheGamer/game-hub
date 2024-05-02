550d1be018c20e13d8008c298865e50f0db8bb8e
MBATheGamer <mbathegamer@gmail.com>
2024-05-02 20:46:07 +0100

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,15 @@
-import { Platform } from "@/hooks/Platform";
+import { Genre } from "@/entities/Genre";
+import { Platform } from "@/entities/Platform";
+import { Publisher } from "@/entities/Publisher";
 
 export type Game = {
   id: number;
   name: string;
   slug: string;
-  description_raw: string;
+  genres: Genre[];
+  publishers: Publisher[];
   metacritic: number;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   rating_top: number;
